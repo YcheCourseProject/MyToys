ddbf3992e2c297ea4d5a74854a7b8868ffce4eb0
CHE Yulin <191836400@qq.com>
2015-12-31 21:19:10 +0800

    intial fuction finished

---
+++ C#/EmailProcess/EmailProcess/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace EmailProcess
+{
+
+    class Program
+    {
+        static OleDbCommand GetUpdateSql(String companyName, String emailAddr)
+        {
+            OleDbCommand oleDbCommand = new OleDbCommand();
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.Append(@"update [匹配公司名和邮箱地址] set [Remark] = 'Company Email' ").
+                Append("where [Company Name] = ").Append("@companyName").
+                Append(" and [Email address] = ").Append("@emailAddr");
+            oleDbCommand.CommandText = strBuilder.ToString();
+            oleDbCommand.Parameters.AddWithValue("@companyName", companyName);
+            oleDbCommand.Parameters.AddWithValue("@emailAddr", emailAddr);
+            return oleDbCommand;
+        }
+
+        static void Main(string[] args)
+        {
+            String ConStr = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
+            @"Data source= E:\" +
+            @"email_info.accdb";
+            int rowsAffectedNum = 0;
+            OleDbConnection oleCon = new OleDbConnection(ConStr);
+            OleDbDataAdapter oleDap = new OleDbDataAdapter("select * from [匹配公司名和邮箱地址]", oleCon);
+            DataSet ds = new DataSet();
+            oleDap.Fill(ds);
+            Console.WriteLine(ds.Tables[0].Columns.Count);
+            oleCon.Open();
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                String emailAddrStr = row["Email address"].ToString();
+                emailAddrStr = Regex.Replace(emailAddrStr, "-", "");
+                if (emailAddrStr.Contains("@"))
+                {
+                    String rightStr = Regex.Split(emailAddrStr, "@")[1];
+                    rightStr = Regex.Split(rightStr, "\\.")[0];
+                    StringBuilder strBuilder = new StringBuilder();
+                    foreach (Char char_in_string in rightStr)
+                    {
+                        strBuilder.Append(char_in_string).Append(".*");
+                    }
+                    Regex r = new Regex(strBuilder.ToString(), RegexOptions.IgnoreCase);
+                    String nameStr = row["Company Name"].ToString();
+                    Match m = r.Match(nameStr);
+                    if (m.Success)
+                    {
+                        OleDbCommand oleCommand = GetUpdateSql(row["Company Name"].ToString(), row["Email address"].ToString());
+                        oleCommand.Connection = oleCon;
+                        rowsAffectedNum += oleCommand.ExecuteNonQuery();
+                        if (rowsAffectedNum % 1000 == 0)
+                            Console.WriteLine(rowsAffectedNum);
+                    }
+
+                }
+            }
+
+            oleCon.Close();
+            oleCon.Dispose();
+            Console.Read();
+        }
+    }
+}
---
+++ C#/EmailProcess/EmailProcess/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EmailProcess")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HKUST")]
+[assembly: AssemblyProduct("EmailProcess")]
+[assembly: AssemblyCopyright("Copyright © HKUST 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2f3d13d3-ddd6-4978-b1ad-44756b2cee54")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
