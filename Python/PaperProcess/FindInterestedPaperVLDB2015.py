0a23fcf2727a369a7161b527fb82124eda0f8649
Che Yulin <191836400@qq.com>
2016-01-11 11:23:03 +0800

    download vldb 2013,4,5

--- Python/PaperProcess/FindInterestedPaperVLDB2015.py
+++ Python/PaperProcess/FindInterestedPaperVLDB2015.py
@@ -1,11 +1,10 @@
-__author__ = 'cheyulin'
-
 import urllib2
-from StringIO import StringIO
-from bs4 import BeautifulSoup
 import gzip
 import re
+from StringIO import StringIO
+from bs4 import BeautifulSoup
 
+__author__ = 'cheyulin'
 
 def loadData(url):
     request = urllib2.Request(url)
@@ -26,9 +25,18 @@ if __name__ == '__main__':
     page = loadData('http://www.vldb.org/2015/program/lib/FullProgram.html#D1F1045T1215R5')
     soup = BeautifulSoup(page, from_encoding='GB18030')
     paper_info = soup.find_all('div', {'class': 'booklet_paper_folded'});
+    path = r'/home/cheyulin/Documents/Paper/GraphInDB/'
     for paper in paper_info:
         info = paper.findAll('strong')
         title = info[0]
-        if (re.match(r'.*Graph.*', str(title))):
-            print title
-            # print soup.prettify().encode('GB18030')
+        if re.match(r'.*Graph.*', str(title)):
+            pdf_img = paper.findAll('img', {'class': 'paper_controls'})
+            if re.match(r'.*http.*pdf.*', str(pdf_img)):
+                paper_file_name = str(title).replace('<strong>', '').replace('</strong>', '').replace(' ', '_') + '.pdf'
+                print paper_file_name
+                paper_url = re.search(r'http.*[^"]pdf', str(pdf_img)).group(0)
+                print paper_url
+                f = urllib2.urlopen(paper_url)
+                with open(path+paper_file_name,'wb') as output_stream:
+                    output_stream.write(f.read())
+                # print soup.prettify().encode('GB18030')
