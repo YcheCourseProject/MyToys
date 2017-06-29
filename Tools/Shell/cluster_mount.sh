3d45c2c71b06edbfe084c09aeea448ddcad97919
CHE Yulin <yche@cse.ust.hk>
2017-06-29 21:52:40 +0800

    upload pic

--- Tools/Shell/cluster_mount.sh
+++ Tools/Shell/cluster_mount.sh
@@ -1,1 +1,3 @@
-sudo sshfs -o allow_other,IdentityFile=~/.ssh/id_rsa yche@gpu1.ust.hk: /mnt/mount-gpu
+sudo sshfs -o allow_other,IdentityFile=~/.ssh/id_rsa yche@gpu1.ust.hk:/ /mnt/mount-gpu
+sudo sshfs -o allow_other,IdentityFile=~/.ssh/id_rsa yche@luocpu8.ust.hk:/ /mnt/luocpu8
+sudo sshfs -d -o allow_other -o reconnect -o ServerAliveInterval=15 yche@luocpu8.ust.hk:/ /mnt -p 12345 -C
---
+++ Tools/Shell/mount_luocpu8.sh
@@ -0,0 +1,1 @@
+sudo sshfs -o allow_other,IdentityFile=~/.ssh/id_rsa yche@luocpu8.cse.ust.hk: /mnt/luocpu8
